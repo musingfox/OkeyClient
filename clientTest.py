85faa7a43346eaf1608dfcd6fdbac92af28ce25b
jubeatWwW <l6104400@gmail.com>
2016-01-26 03:22:12 +0800

    Test if server working properly

--- clientTest.py
+++ clientTest.py
@@ -10,7 +10,19 @@ sock.connect(("140.113.235.151",7975))
 data = {"command":"game","setting":{"log":"verbose"}}
 
 sock.sendall(json.dumps(data))
+sock.recv(2048)
 
+sock.sendall(json.dumps({"player":1,"action":"hand"}))
+hand = json.loads(sock.recv(2048))
+print(hand['cards']['hand'])
+
+sock.sendall(json.dumps({"player":1,"action":"take","from":"deck"}))
+takeCard = json.loads(sock.recv(2048))
+print("\n\n\nTake:\n"+str(takeCard['card']))
+
+sock.sendall(json.dumps({"player":1,"action":"throw","cards":hand['cards']['hand']}))
 print(sock.recv(2048))
-
+print("\n\n")
+sock.sendall(json.dumps({"player":2,"action":"hand"}))
+print(sock.recv(2048))
 
