7d71208e62dc7f47d11484bbe5a9d633f710749a
archiexdex <rc840327@gmail.com>
2016-01-26 04:05:36 +0800

    change: hand top function: getcard ..

--- client.cpp
+++ client.cpp
@@ -12,56 +12,120 @@
 #include <typeinfo>
 #include <map>
 #include <string>
+#include <utility>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct color{
-	string red=(const char*)"red",black=(const char*)"black",yellow=(const char*)"yellow",blue=(const char*)"blue";
-};
-map<string,int > card;
+#define red 0
+#define black 1
+#define yellow 2
+#define blue 3
+
+const char* Color[] = {"red", "black", "yellow" , "blue" };
+
+pair<string,int> top[5];
+int hand[4][14];
 char tmp[2048];
 
-void init(char* S,string key1 , string value1 , string key2 , string key3 , string value2){
-	const char* k1 = key1.c_str() , *v1 = value1.c_str() , *k2 = key2.c_str() , *v2 = value2.c_str() , *k3 = key3.c_str();
-	sprintf(S,"{\"%s\":\"%s\",\"%s\":{\"%s\":\"%s\"}}",k1,v1,k2,k3,v2);
-//	sprintf(S,"{\"%s\": {\"%s\": \"%s\"}, \"%s\": \"%s\"}",k2,k3,v2,k1,v1);
-}
-void get(char* S,string key1,string value1,string key2,string value2){
-	const char *k1=key1.c_str() , *v1=value1.c_str(), *k2=key2.c_str(), *v2=value2.c_str();
-	sprintf(S,"{\"%s\": \"%s\",\"%s\": \"%s\"}",k1,v1,k2,v2);
-}
-void takecard(char* S, string key1, string value1, string key2, string value2, string key3, string value3){
-	const char *k1=key1.c_str(), *v1=value1.c_str(), *k2=key2.c_str(), *v2=value2.c_str(), *k3=key3.c_str(), *v3=value3.c_str();
-	sprintf(S,"{\"%s\":\"%s\",\"%s\":\"%s\",\"%s\":\"%s\"}",k1,v1,k2,v2,k3,v3);
-}
 
-string getcard(char* S){
-	string tmp="";printf("%s\n",S);
-	int st = 0;
-	for( st = 0 ; st < strlen(S) ; st++ ){
-		if( S[st] == '\"' && S[st+1] == 'c' ) break;
+
+void next(char* &ptr){
+	ptr = strtok(NULL," \",:[]{}");
+}
+void buildmap(string tmp,int num){
+	const char* color = tmp.c_str();
+	if( !strcmp(color,Color[red]) )
+		hand[red][num]++;
+	else if( !strcmp(color,Color[black]) )
+		hand[black][num]++;
+	else if( !strcmp(color,Color[yellow]) )
+		hand[yellow][num]++;
+	else if( !strcmp(color,Color[blue]) )
+		hand[blue][num]++;
+}
+char* getcard(char* S){
+	string color;
+	int num;
+	char *ptr=NULL;
+	
+	ptr = strtok(S," \",:[]{}");
+	while( ptr != NULL ){
+		printf("%s$$$$$$$\n",ptr);
+		if( !strcmp(ptr,"hand") ){
+			next(ptr);
+			for( int i = 0 ; i < 24 ; i++,next(ptr) ){
+				if( !strcmp(ptr,"color") ){
+					next(ptr);
+					color = ptr;
+					next(ptr),next(ptr);
+					num = atoi(ptr);
+					buildmap(color,num);
+				}
+			}
+		}
+		else if ( !strcmp(ptr,"top") ){
+			next(ptr);
+			if( !strcmp(ptr,"player1") ){
+				next(ptr),next(ptr);
+				color = ptr;
+				next(ptr),next(ptr);
+				num = atoi(ptr);
+				pair<string,int> tmp(color,num);
+				top[1] = tmp;
+			}
+			else if( !strcmp(ptr,"player2") ){
+				next(ptr),next(ptr);
+				color = ptr;
+				next(ptr),next(ptr);
+				num = atoi(ptr);
+				pair<string,int> tmp(color,num);
+				top[2] = tmp;
+			}
+			else if( !strcmp(ptr,"player3") ){
+				next(ptr),next(ptr);
+				color = ptr;
+				next(ptr),next(ptr);
+				num = atoi(ptr);
+				pair<string,int> tmp(color,num);
+				top[3] = tmp;
+			}
+			else if( !strcmp(ptr,"player4") ){
+				next(ptr),next(ptr);
+				color = ptr;
+				next(ptr),next(ptr);
+				num = atoi(ptr);
+				pair<string,int> tmp(color,num);
+				top[4] = tmp;
+			}
+		}
+		
+		next(ptr);
 	}
 	
-	for( ; st < strlen(S)-1 ; st++ ){
-		tmp += S[st];
-	}
 	return tmp;
 }
-
-void throwcard(char* S,string key1, string value1, string key2, string value2, string c){
-	const char *k1=key1.c_str(), *v1=value1.c_str(), *k2=key2.c_str(), *v2=value2.c_str(), *card=c.c_str();
+void init(char* S,const char* k1 ,const char* v1 ,const char* k2 ,const char* k3 ,const char* v2){
+	sprintf(S,"{\"%s\":\"%s\",\"%s\":{\"%s\":\"%s\"}}",k1,v1,k2,k3,v2);
+}
+void get(char* S,const char* k1,const char* v1,const char* k2,const char* v2){
+	sprintf(S,"{\"%s\": \"%s\",\"%s\": \"%s\"}",k1,v1,k2,v2);
+}
+void takecard(char* S,const char* k1,const char* v1,const char*k2,const char*v2,const char* k3,const char* v3){
+	sprintf(S,"{\"%s\":\"%s\",\"%s\":\"%s\",\"%s\":\"%s\"}",k1,v1,k2,v2,k3,v3);
+}
+void throwcard(char* S,const char* k1,const char* v1,const char* k2,const char* v2,const char* card){
 	sprintf(S,"{\"%s\":\"%s\", \"%s\":\"%s\",%s}",k1,v1,k2,v2,card);
 }
-
 int main(int argc, char* argv[]){
 	int soc , s , rec , con , state = 0;
 	char *IP = argv[1] , *port = argv[2];
 	char S[2048] , buf[2048] ;
-	string cards;
-	
+	char* cards;
+
 	sockaddr_in server , client;
 	socklen_t len = sizeof( sockaddr_in );
 	memset( &server , 0 , sizeof (server) );
-
+	
 	server.sin_family = AF_INET;
 	server.sin_port = htons( atoi((const char*)port) );
 	server.sin_addr.s_addr = inet_addr( IP );
@@ -76,49 +140,42 @@ int main(int argc, char* argv[]){
 	printf("#set up\n");
 	printf("TCP Server listening on %s:%s\n\n",IP,port);
 	
+	bool first = true;
+	int player = 0;
 	state = 0;
 	while( 1 ){
 		memset(S,0,sizeof(S) );
 		memset(buf,0,sizeof(buf) );
-		switch(state){
-			case 0:
-				init(S,"command","game","setting","log","verbose");
-				state = 1;
-				break;
-			case 1:
-				get(S,"player","1","action","hand");
-				state = 2;
-				break;
-			case 2:
-				cards = getcard(tmp);
-				takecard(S,"player","1","action","take","from","deck");
-				state = 3;
-				break;
-			case 3:
-				throwcard(S,"player","1","action","throw",cards);
-				state = 4;
-				break;
-			case 4:
-				get(S,"player","2","action","hand");
-				state = 5;
-				break;
-			case 5:
-				cards = getcard(tmp);
-				takecard(S,"player","2","action","take","from","deck");
-				state = 6;
-				break;
-			case 6:
-				throwcard(S,"player","2","action","throw",cards);
-				state = 7;
-				break;
-				
+		if( first ){
+			init(S,"command","game","setting","log","separate");
+			first = false;
 		}
-		scanf("%*s");
+		else {
+			const char* now = to_string(player+1).c_str();
+			switch(state){
+				case 0:
+					get(S,"player",now,"action","hand");
+					state = 1;
+					break;
+				case 1:
+					memset(hand,0,sizeof(hand) );
+					cards = getcard(tmp);
+					takecard(S,"player",now,"action","take","from","deck");
+					state = 2;
+					break;
+				case 2:
+					throwcard(S,"player",now,"action","throw",cards);
+					player = (player+1)%4;
+					state = 0;
+					break;
+			}
+		}
+		
 		printf("%s~~~~~~~~~~~~~~\n",S);
 		s = send(soc , S , sizeof(char)*strlen(S) , 0 );
 		rec = recv(soc , buf , sizeof(buf) , 0 );
 		printf("%s!!!!!!!!!!!!!!\n",buf);sprintf(tmp,"%s",buf);
-	//	scanf("%*s");
+		scanf("%*s");
 				
 	}
 	//	close(soc);
